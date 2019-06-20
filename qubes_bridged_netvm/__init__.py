3140bf05252adbf2eb3cad124a98b49b6b5fe346
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-06-20 19:04:37 +0200

    Handle new network config format and attach network directly on domain start

--- qubes_bridged_netvm/__init__.py
+++ qubes_bridged_netvm/__init__.py
@@ -22,23 +22,80 @@
 import qubes.ext
 import asyncio
 import re
+import libvirt
 
 
 def check_mac(mac):
+    """Check MAC format."""
     mac_regex = re.compile('^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
     return bool(re.match(mac_regex, mac))
+
+
+def get_bridged_ifaces(vm):
+    """Get bridged interfaces from features"""
+    ifaces = []
+
+    bridge_macs = sorted(list(set([str(br).split('_')[1] for br in vm.features if 'bridge_' in br])))
+    for mac in bridge_macs:
+        if check_mac(mac):
+            name = vm.features.get('bridge_' + mac + '_name', '')
+            backenddomain = vm.features.get('bridge_' + mac + '_backenddomain', '')
+
+            if name and backenddomain:
+                bridge = {
+                    'mac': mac.lower(),
+                    'name': vm.features.get('bridge_' + mac + '_name', ''),
+                    'backenddomain': vm.features.get('bridge_' + mac + '_backenddomain', ''),
+                    'ip': vm.features.get('bridge_' + mac + '_ip', ''),
+                    'netmask': vm.features.get('bridge_' + mac + '_netmask', ''),
+                    'gateway': vm.features.get('bridge_' + mac + '_gateway', '')
+                }
+
+                ifaces.append(bridge)
+            else:
+                vm.log.warning(
+                    'QubesBridgedNetVMExtension: missing bridge name or backenddomain for interface {}'.format(mac))
+        else:
+            vm.log.warning('QubesBridgedNetVMExtension: invalid MAC {}'.format(mac))
+
+    return ifaces
+
+
+def attach_bridged_network(vm, bridge):
+    """Attach network in this machine to it's bridged NetVM."""
+
+    if vm.is_running():
+        if vm.app.domains[bridge['backenddomain']] is None:
+            raise qubes.exc.QubesVMError(vm, 'Bridged NetVM {} not found'.format(vm.app.domains[bridge['backenddomain']]))
+
+        bridge_xml = '''
+            <interface type="bridge">
+                <source bridge="{name}" />
+                <mac address="{mac}" />
+                <backenddomain name="{backenddomain}" />
+                <script path="vif-bridge" />
+            </interface>
+            '''.format(name=bridge['name'], mac=bridge['mac'], backenddomain=bridge['backenddomain'])
+
+        vm.libvirt_domain.attachDevice(bridge_xml)
 
 
 class QubesBridgedNetVMExtension(qubes.ext.Extension):
     """qubes-core-admin extension for handling Bridged NetVM"""
 
+    @qubes.ext.handler('domain-qdb-create')
+    def on_qdb_create(self, vm, event):
+        for bridge in get_bridged_ifaces(vm):
+            vm.untrusted_qdb.write('/net-config/' + bridge['mac'] + '/ip', bridge['ip'])
+            vm.untrusted_qdb.write('/net-config/' + bridge['mac'] + '/netmask', bridge['netmask'])
+            vm.untrusted_qdb.write('/net-config/' + bridge['mac'] + '/gateway', bridge['gateway'])
+
     @qubes.ext.handler('domain-pre-start')
     @asyncio.coroutine
     def on_domain_pre_start(self, vm, event, start_guid, **kwargs):
-        if 'bridge_backenddomain' in vm.features:
-            backenddomain = vm.features['bridge_backenddomain']
+        for bridge in get_bridged_ifaces(vm):
             try:
-                bridge_backenddomain = vm.app.domains[backenddomain]
+                bridge_backenddomain = vm.app.domains[bridge['backenddomain']]
             except KeyError:
                 vm.log.error('QubesBridgedNetVMExtension: Bridged NetVM not provided.')
                 return
@@ -47,14 +104,11 @@ class QubesBridgedNetVMExtension(qubes.ext.Extension):
                 if not bridge_backenddomain.is_running():
                     yield from bridge_backenddomain.start(start_guid=start_guid, notify_function=None)
 
-    @qubes.ext.handler('domain-qdb-create')
-    def on_qdb_create(self, vm, event):
-        mac = vm.features.get('bridge_mac', '').lower()
-        ip = vm.features.get('bridge_ip', '')
-        netmask = vm.features.get('bridge_netmask', '')
-        gateway = vm.features.get('bridge_gateway', '')
-
-        if check_mac(mac):
-            vm.untrusted_qdb.write('/net-config/' + mac + '/ip', ip)
-            vm.untrusted_qdb.write('/net-config/' + mac + '/netmask', netmask)
-            vm.untrusted_qdb.write('/net-config/' + mac + '/gateway', gateway)
+    @qubes.ext.handler('domain-start')
+    def on_domain_start(self, vm, event, start_guid, **kwargs):
+        for bridge in get_bridged_ifaces(vm):
+            vm.log.info('Attaching bridged network {mac}'.format(mac=bridge['mac']))
+            try:
+                attach_bridged_network(vm, bridge)
+            except (qubes.exc.QubesException, libvirt.libvirtError):
+                vm.log.warning('Cannot attach network {mac}'.format(mac=bridge['mac']), exc_info=1)
